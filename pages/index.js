2f17ee68b6e7ed5c3b10defc36383aa6560e4873
Glauber Lima Cantacini Romano <gcantacini@hotmail.com>
2024-10-07 16:37:19 +0000

    Modifica a frase da página com a tag correta

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Trabalhadores de todo o mundo, uni-vos!</h1>;
+    return <div>
+        <h1>Os filósofos têm apenas <em>interpretado</em> o mundo de maneiras diferentes; a questão, porém, é <em>transformá-lo</em>.</h1>
+        <h2><em>Tese onze</em></h2>
+    </div>; 
 }
 
 export default Home;
