745d4dae657a1dae4948a2c02a78625870756bad
Glauber Lima Cantacini Romano <gcantacini@hotmail.com>
2024-10-09 12:54:56 +0000

    Adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprendizado do curso do Filipe Deschamps em https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 function Home() {
-    return <div>
-        <h1>Os filósofos têm apenas <em>interpretado</em> o mundo de maneiras diferentes; a questão, porém, é <em>transformá-lo</em>.</h1>
-        <h2><em>Tese onze</em></h2>
-    </div>; 
+  return (
+    <div>
+      <h1>
+        Os filósofos têm apenas <em>interpretado</em> o mundo de maneiras
+        diferentes; a questão, porém, é <em>transformá-lo</em>.
+      </h1>
+      <h2>
+        <em>Tese onze</em>
+      </h2>
+    </div>
+  );
 }
 
 export default Home;
